5ecb941b1b7a0b16d871966a600fa906fb83f6a1
Long Chu Hai <long.ch@sitv.com.vn>
2024-07-16 16:58:38 +0700

    update

--- lib/src/pages/navigator.dart
+++ lib/src/pages/navigator.dart
@@ -2,6 +2,7 @@ import 'package:chu_hai_long_flutter_10/src/pages/cart/cart.dart';
 import 'package:chu_hai_long_flutter_10/src/pages/favorite/favorite.dart';
 import 'package:chu_hai_long_flutter_10/src/pages/profile/profile.dart';
 import 'package:chu_hai_long_flutter_10/src/pages/shop.dart';
+import 'package:chu_hai_long_flutter_10/src/pages/stream/download.dart';
 import 'package:flutter/cupertino.dart';
 
 class NavigatorBuilder extends StatefulWidget {
@@ -50,7 +51,7 @@ class _NavigatorBuilderState extends State<NavigatorBuilder> {
         return switch (index) {
           0 => CupertinoTabView(
               // defaultTitle: ShopPage.title,
-              builder: (context) => ShopPage(
+              builder: (context) => DownloadPage(
                 key: shopKey,
               ),
             ),
---
+++ lib/src/pages/stream/download.dart
@@ -0,0 +1,97 @@
+import 'dart:async';
+import 'dart:math';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DownloadPage extends StatefulWidget {
+  static const title = "Profile";
+  static const icon = ImageIcon(
+    AssetImage("lib/src/assets/images/user.png"),
+    color: Color.fromARGB(255, 252, 252, 252),
+  );
+  const DownloadPage({
+    super.key,
+  });
+
+  @override
+  State<DownloadPage> createState() => _DownloadPageState();
+}
+
+class _DownloadPageState extends State<DownloadPage> {
+  final _items = <String>[];
+  final _random = Random();
+  final _streamController = StreamController<String>();
+
+  @override
+  void dispose() {
+    _streamController.close();
+    super.dispose();
+  }
+
+  void _addItemStream() async {
+    int delay = _random.nextInt(5) + 10;
+    print(delay);
+    int start = 0;
+    while (start < delay) {
+      if (start >= delay) {
+        continue;
+      }
+      start += 2;
+      await Future.delayed(const Duration(seconds: 2));
+      String newItem = 'file download no ${_items.length + 1}';
+      _streamController.add(newItem);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return Padding(
+      padding: const EdgeInsets.all(40),
+      child: Column(
+        children: [
+          Expanded(
+            child: StreamBuilder<String>(
+              stream: _streamController.stream,
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                }
+
+                if (!snapshot.hasData) {
+                  return Text('');
+                }
+
+                _items.add(snapshot.data!);
+                return ListView.builder(
+                  itemCount: _items.length,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        ListTile(
+                      title: Text(_items[index]),
+                    ),
+                    Center(
+                      child: CircularProgressIndicator(
+                          semanticsLabel: _items.length.toString()))
+                      ],
+                    )
+                  },
+                );
+              },
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () => _addItemStream(),
+            child: const Text('Download'),
+          ),
+        ],
+      ),
+    );
+  }
+}
